c8d1178fc9607b1f57201bea70d121050f407d11
abdeahmed <a.khalfalla151@gmail.com>
2025-01-24 18:25:49 +0300

    check cell function added

--- checkcell.go
+++ checkcell.go
@@ -1,8 +1,20 @@
 package sudoku
 
 func CheckCell(grid [][]int, row int, col int, num int) bool {
-	/*
-		this function will check if the number tested in the empty cell
-		is a viable option
-	*/
+    for i := 0; i < 9; i++ {
+        if grid[row][i] == num || grid[i][col] == num {
+            return false
+        }
+    }
+
+    startRow := (row / 3) * 3
+    startCol := (col / 3) * 3
+    for i := startRow; i < startRow+3; i++ {
+        for j := startCol; j < startCol+3; j++ {
+            if grid[i][j] == num {
+                return false
+            }
+        }
+    }
+    return true
 }
