e6ebe84bf7a946a61bcf35e1d1652d8d79ca987a
abdeahmed <a.khalfalla151@gmail.com>
2025-01-24 17:56:21 +0300

    pushed the sudoku solver algorithm

--- solvegrid.go
+++ solvegrid.go
@@ -6,4 +6,22 @@ func SolveGrid(grid [][]int) bool {
 		and returns true if the grid is filled meaning there is a valid solution
 		or false if it exhausted all posiable combinations
 	*/
+
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if grid[i][j] == 0 {
+				for num := 1; num <= 9; num++ {
+					if CheckCell(grid, i, j, num) {
+						grid[i][j] = num
+						if SolveGrid(grid) {
+							return true
+						}
+						grid[i][j] = 0
+					}
+				}
+				return false
+			}
+		}
+	}
+	return true
 }
